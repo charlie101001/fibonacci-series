96e1437987a6df4d01d6dc336687d9d80157324c
charlie101001 <63311287+charlie101001@users.noreply.github.com>
2020-07-13 01:35:35 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,12 +1,10 @@
-a = int(input("enter lenght of series"))
-f=0
-s=1
-if a<0:
-    print("wrong input")
-else :
-    print(f,s,end=" ")
-    for x in range(2,a):
-        next=f+s                           
-        print(next,end=" ")
-        f=s
-        s=next
+list1=[12,-7,5,64,-14]
+for x in list1:
+    if x>0:
+        print(x)
+
+list2=[12,14,-95,3]
+for y in list2:
+    if y>0:
+        print(y)    
+    
